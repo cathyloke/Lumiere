f83299d665422a50e40b3fafc35431f92830202d
cathyloke <catloke963@gmail.com>
2024-08-28 10:07:34 +0800

    Added the dependencies

---
+++ react-native.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    project: {
+        ios:{},
+        android:{}
+    },
+    assets:['./assets/fonts/'],
+    dependencies: {
+        "react-native-sqlite-storage": {
+          platforms: {
+            android: {
+              sourceDir:
+                "../node_modules/react-native-sqlite-storage/platforms/android-native",
+              packageImportPath: "import io.liteglue.SQLitePluginPackage;",
+              packageInstance: "new SQLitePluginPackage()"
+            }
+          }
+        }
+      }
+  }
