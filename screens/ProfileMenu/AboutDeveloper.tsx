49121d8d85123fd8a8b02913a9bdaf559f25c870
joelytin <joelytin@outlook.com>
2024-09-05 13:26:02 +0800

    Adjust About Developers page

--- screens/ProfileMenu/AboutDeveloper.tsx
+++ screens/ProfileMenu/AboutDeveloper.tsx
@@ -11,12 +11,12 @@ const AboutDeveloper = ({ navigation }: any) => {
       <Text style={generalStyles.header}>Lumière was developed by a group of students in UTAR Sungai Long in September 2024.</Text>
       <Text style={generalStyles.header}>Lumière Team: </Text>
       <View>
-        <Text style={generalStyles.header}>Choo Jia Zheng</Text>
-        <Text style={generalStyles.header}>Leong Ming Shan</Text>
-        <Text style={generalStyles.header}>Loke Weng Yan</Text>
-        <Text style={generalStyles.header}>Tin Hui Hui</Text>
+        <Text style={generalStyles.aboutDevText}>Choo Jia Zheng</Text>
+        <Text style={generalStyles.aboutDevText}>Leong Ming Shan</Text>
+        <Text style={generalStyles.aboutDevText}>Loke Weng Yan</Text>
+        <Text style={generalStyles.aboutDevText}>Tin Hui Hui</Text>
       </View>
-      <Text style={generalStyles.header}>Copyright © 2024 Lumière Team. All rights reserved.</Text>
+      <Text style={generalStyles.aboutDevCopyright}>Copyright © 2024 Lumière Team. All rights reserved.</Text>
     </View>
   );
 };
